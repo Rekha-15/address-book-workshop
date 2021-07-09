854e395dcdab0b0b54b43b4b24375368b7b9e840
Rekha-15 <rekhapatil.1509@gmail.com>
2021-07-09 08:31:27 -0400

    added : welcome message : .

---
+++ src/main/java/AddressBookWorkshop/AddressBook.java
@@ -0,0 +1,22 @@
+/************************************************************
+ * @Purpose Implementing Address Book Problem .
+ * @author Rekha
+ * @version 1.0
+ * @since 9/07/2021
+ **************************************************************/
+package AddressBookWorkshop;
+
+/**
+ * main method,Printing welcome statement
+ * 
+ * @author rekha
+ *
+ */
+public class AddressBook {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Address Book Program");
+	}
+
+}
